ff063e8790a4f3a61c28f7ef185c880a682a8869
Nicolas Lima <nicolaslimacontato@outlook.com>
2024-03-15 20:43:48 -0300

    Finish :#

--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
+// menu hamburguer
+$("i").click(function () {
+    $("ul").toggleClass("open")
+})
+
 //dropdown
 $(function () {
     $('.navBar li').hover(
@@ -34,3 +39,55 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev"
     }
 })
+
+// cards
+if ($(window).width() < 769) {
+    var swiper = new Swiper(".aboutInner", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+    });
+} else {
+    var swiper = new Swiper(".aboutInner", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+}
+
+//about
+//reload
+window.addEventListener('resize', function (event) {
+    recarrega();
+});
+
+function recarrega() {
+    document.location.reload();
+};
+
+// aos
+
+AOS.init({
+    duration: 1800,
+});
